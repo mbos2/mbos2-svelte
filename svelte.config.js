416e71f4ef0df2b4da133d3b4294ff634828d8c8
Matej Bošnjak <mbosnjak2@outlook.com>
2020-12-13 13:56:06 +0100

    Added tailwindcss and its support

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,9 @@ const autoPreprocess = require('svelte-preprocess');
 
 module.exports = {
   preprocess: autoPreprocess({
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    },
     defaults: {
       script: 'typescript',
     },
