f7a0952002e8f8e916d4a35f9b5b709542487fcf
Matej Bošnjak <mbosnjak2@outlook.com>
2020-12-13 01:13:26 +0100

    Added component transition

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -1,17 +1,27 @@
 <script lang='typescript'>
+	import { scale } from 'svelte/transition';
 	import {Route} from 'tinro';	
     import {mainRoutes} from '@components/routing/routes';
 	import Navigation from '@components/global/Navigation.svelte';
 </script>  
 
-<style>
+<style global>
+	h1 {
+		color: red;
+	}
 
+	.bd {
+		border: 1px solid red;
+	}
 </style>
+
 <Navigation />
 <div class="a">
 	{#each mainRoutes as route }		
 		<Route path="{route.url}">	
-			<svelte:component this={route.component} />		
+			<div in:scale="{{ duration: 2000}}" out:scale="{{duration: 2000 }}">
+				<svelte:component this={route.component} />	
+			</div>
 		</Route>	
 	{/each}
 </div>
